395ee2010e1c195b84670840a446bee996f6e73a
hero-goku <samantharobertz@outlook.com>
2025-09-08 16:06:50 +0700

    Add files via upload

--- ubuntusysteminstall.sh
+++ ubuntusysteminstall.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt update
 sudo apt install ibus-bamboo ibus bleachbit kazam python3-pip yt-dlp neofetch spice-vdagent flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo apt autopurge gnome-keyring libpam-gnome-keyring gstreamer1.0-vaapi firefox thunderbird libreoffice-common timeshift celluloid rhythmbox libreoffice-uiconfig-common libreoffice-uiconfig-writer libreoffice-uiconfig-calc libreoffice-uiconfig-impress libreoffice-uiconfig-draw gnome-calendar gnome-calculator xviewer, xreader drawing hypnotix xed snapd kwrite gwenview kontact
+sudo apt autopurge gnome-keyring libpam-gnome-keyring gstreamer1.0-vaapi firefox thunderbird libreoffice-common timeshift celluloid rhythmbox libreoffice-uiconfig-common libreoffice-uiconfig-writer libreoffice-uiconfig-calc libreoffice-uiconfig-impress libreoffice-uiconfig-draw gnome-calendar gnome-calculator xviewer xreader drawing hypnotix xed snapd kwrite gwenview kontact
 sudo apt upgrade
 sudo apt autopurge
 sudo e4defrag /home
